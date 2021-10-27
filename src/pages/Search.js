96a03b61a1b2c5df238f35bc26549a2aaab993ac
Vitor <tutornews2@gmail.com>
2021-10-27 20:26:17 -0300

    Adiciona requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,56 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      banda: '',
+      isButtonDSearch: true,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.buttonSearch = this.buttonSearch.bind(this);
+    this.checkButtonSearch = this.checkButtonSearch.bind(this);
+  }
+  // requesito 5 muito parecido com o do Login
+
+  handleChange({ target }) {
+    const valueDigitado = target.value;
+    this.setState({ banda: valueDigitado }, () => this.checkButtonSearch());
+  }
+
+  buttonSearch() {
+
+  }
+
+  checkButtonSearch() {
+    const { banda } = this.state;
+    if (banda.length >= Number('2')) {
+      this.setState({ isButtonDSearch: false });
+    }
+  }
+
   render() {
+    const { isButtonDSearch } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <label htmlFor="search-artist-input">
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            id="search-artist-input"
+            onChange={ this.handleChange }
+            placeholder="Digite a banda"
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            onClick={ this.buttonSearch }
+            disabled={ isButtonDSearch }
+          >
+            Pesquisar
+          </button>
+        </label>
       </div>
     );
   }
