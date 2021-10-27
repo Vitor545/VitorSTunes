91e71d2c8986bf109fe705cb8a52ec91f271b946
Vitor <tutornews2@gmail.com>
2021-10-27 19:23:28 -0300

    Adiciona Requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './ Loading';
 
@@ -7,25 +8,33 @@ export default class Header extends Component {
     super(props);
     this.state = {
       loading: true,
-      user: '',
+      userName: '',
     };
   }
 
+  // Muito pararecido com o requesito 2 somente muda que tem que colocar componentDidMount para que
+  // no primeiro ciclo de vida o recuperaUser rode.
   componentDidMount() {
     this.recuperaUser();
   }
 
   async recuperaUser() {
-    const user = await getUser();
-    this.setState({ user }, () => this.setState({ loading: false }));
+    const userName = await getUser();
+    this.setState({ userName }, () => this.setState({ loading: false }));
   }
 
   render() {
-    const { loading, user } = this.state;
+    const { loading, userName } = this.state;
     return loading ? <Loading /> : (
       <div>
         <header data-testid="header-component">
-          <p data-testid="header-user-name">{user.name}</p>
+          <p data-testid="header-user-name">{userName.name}</p>
+          {
+            // Source do Link: https://reactrouter.com/web/api/Link, bastante coisa interessante
+          }
+          <Link to="/search" data-testid="link-to-search">Search</Link>
+          <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+          <Link to="/profile" data-testid="link-to-profile">Profile</Link>
         </header>
       </div>
     );
