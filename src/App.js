e19c19a2349c9e365e249c8b4511efa5a5317156
Vitor <tutornews2@gmail.com>
2021-10-27 18:10:28 -0300

    Adiciona requisito 2

--- src/App.js
+++ src/App.js
@@ -8,13 +8,64 @@ import Favorites from './pages/Favorites';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import NotFound from './pages/NotFound';
+import { createUser } from './services/userAPI';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: '',
+      isButtonDisable: true,
+      loading: false,
+      irSearch: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.checkButton = this.checkButton.bind(this);
+    this.onClickButton = this.onClickButton.bind(this);
+  }
+
+  handleChange({ target }) {
+    const valueDigitado = target.value;
+    this.setState({ userName: valueDigitado }, () => this.checkButton());
+  }
+
+  onClickButton() {
+    const { userName } = this.state;
+    this.setState({ loading: true },
+      () => createUser({ name: userName }).then(() => this.setState({
+        loading: false,
+        irSearch: true,
+      })));
+  }
+
+  checkButton() {
+    const { userName } = this.state;
+    if (userName.length >= Number('3')) {
+      this.setState({ isButtonDisable: false });
+    }
+  }
+
   render() {
+    const {
+      isButtonDisable,
+      loading,
+      irSearch,
+    } = this.state;
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" render={ (props) => <Login { ...props } /> } />
+          <Route
+            exact
+            path="/"
+            render={ (props) => (<Login
+              { ...props }
+              handleChange={ this.handleChange }
+              isButtonDisable={ isButtonDisable }
+              onClickButton={ this.onClickButton }
+              loading={ loading }
+              irSearch={ irSearch }
+            />) }
+          />
           <Route exact path="/album/:id" render={ (props) => <Album { ...props } /> } />
           <Route
             exact
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router';
+import Loading from './ Loading';
 
 export default class Login extends Component {
   render() {
-    return (
+    const {
+      handleChange,
+      isButtonDisable,
+      onClickButton,
+      loading,
+      irSearch } = this.props;
+    if (irSearch) return <Redirect to="/search" />;
+    return loading ? <Loading /> : (
       <div data-testid="page-login">
-        <h1>Login</h1>
+        <label htmlFor="login-name-input">
+          <input
+            type="text"
+            id="login-name-input"
+            onChange={ handleChange }
+            data-testid="login-name-input"
+            placeholder="Digite seu nome"
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="login-submit-button"
+          disabled={ isButtonDisable }
+          onClick={ onClickButton }
+        >
+          Entrar
+        </button>
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  handleChange: PropTypes.func.isRequired,
+  isButtonDisable: PropTypes.bool.isRequired,
+  onClickButton: PropTypes.func.isRequired,
+  loading: PropTypes.bool.isRequired,
+  irSearch: PropTypes.bool.isRequired,
+};
