6a92415210f7277df5b763487507fdac2db4106a
Vitor <tutornews2@gmail.com>
2021-10-27 14:54:49 -0300

    Adiciona requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Switch } from 'react-router';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" render={ (props) => <Login { ...props } /> } />
+          <Route exact path="/album/:id" render={ (props) => <Album { ...props } /> } />
+          <Route
+            exact
+            path="/favorites"
+            render={ (props) => <Favorites { ...props } /> }
+          />
+          <Route exact path="/profile" render={ (props) => <Profile { ...props } /> } />
+          <Route
+            path="/profile/edit"
+            render={ (props) => <ProfileEdit { ...props } /> }
+          />
+          <Route exact path="/search" render={ (props) => <Search { ...props } /> } />
+          <Route exact render={ (props) => <NotFound { ...props } /> } />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
